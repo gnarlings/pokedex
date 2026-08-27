46319cfad60c5b3f83eeecfedff28bdfce337759
Alex Rawlings <arawlings@pop-os.ad.wycliffe.ca>
2026-08-27 11:53:16 -0600

    CH1 L4: REPL

--- src/repl.ts
+++ src/repl.ts
@@ -1,5 +1,21 @@
+import { createInterface } from "node:readline/promises";
+
 export function cleanInput(input: string): string[] {
   const trimmed = (input ?? "").trim();
   if (trimmed === "") return [];
   return trimmed.toLowerCase().split(/\s+/);
+}
+
+export function startREPL() {
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex > ",
+  });
+  rl.prompt();
+  rl.on("line", (line) => {
+    const clean = cleanInput(line);
+    if (clean.length > 0) console.log(`Your command was: ${clean[0]}`);
+    rl.prompt();
+  });
 }
